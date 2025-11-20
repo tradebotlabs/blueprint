3152b724ca5c6db7ef853eb05ff116880f18354d
zeelrupapara <zeelrupapara@gmail.com>
2025-11-20 14:53:34 +0530

    feat: add change the export.sh file

--- export.sh
+++ export.sh
@@ -1,16 +1,19 @@
 export GPRC_HOST=127.0.01
 export GRPC_PORT=3000
+
 export REDIS_URL=0.0.0.0:6379
 export REDIS_PASSWORD=null
+
 export MYSQL_HOST=127.0.0.1
 export MYSQL_PORT=3306
 export MYSQL_USER=vfxuser
 export MYSQL_PASSWORD=root@12345
 export MYSQL_DB=vfxcore
-export MONGO_URI=mongo://127.0.0.1
+
 export LOG_FILE=blueprint.log
-export MYSQL_DATA=mysql/database
-export REDIS_DATA=/home/emran/go/src/gitlab.com/blueprint/redis_data
-export MYSQL_DATA=/home/emran/go/src/gitlab.com/blueprint/mysql_data
-export APP_ROOT=/home/emran/go/src/gitlab.com/blueprint/app
-export APP_LOG=/home/emran/go/src/gitlab.com/blueprint/logs
+
+export MYSQL_DATA=${HOME}/data/blueprint/mysql
+export REDIS_DATA=${HOME}/data/blueprint/redis
+export MYSQL_DATA=${HOME}/data/blueprint/mysql
+export APP_ROOT=${HOME}/data/blueprint/app
+export APP_LOG=${HOME}/data/blueprint/logs
